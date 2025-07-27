22f7ab6f25e8e9e5660e7e5f2b910f355b379282
Ben Brown <benbrown@Bens-Mac-mini.local>
2025-07-27 12:32:09 +0930

    GO ref

--- app/go/[ref]/route.ts
+++ app/go/[ref]/route.ts
@@ -1,9 +1,5 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  request: NextRequest,
-  context: { params: Record<string, string> }
-) {
+export async function GET(request: NextRequest, context: any) {
   return NextResponse.redirect('https://google.com', 307);
 }
