59be92851ec9e92948d61861b110b3dc96430e70
BennyAFF25 <ben@falconx.com.au>
2026-09-20 10:05:50 +0930

    Fix business analytics import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Providers from './Providers';
 import ThemeWrapper from '@/components/ThemeWrapper';
 import NettmarkOfferTracker from '../components/marketing/NettmarkOfferTracker';
-import BusinessProductAnalytics from '@/components/analytics/BusinessProductAnalytics';
+import BusinessProductAnalytics from '../components/analytics/BusinessProductAnalytics';
 import { Toaster } from 'react-hot-toast';
 import Script from 'next/script';
 import { META_PIXEL_ID } from '@/../utils/marketing/metaPixel';
